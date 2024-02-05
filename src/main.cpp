fbdad4a2d9a61ccd790e2318b370e5cb50c117b1
ArkaneOnline <niflheim.exease@gmail.com>
2024-02-05 02:07:38 -0600

    move position of the bade to be similar to the mod bade

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,50 @@
-#include <Geode/utils/web.hpp>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/ProfilePage.hpp>
-#include <Geode/binding/ProfilePage.hpp>
-#include <Geode/Bindings.hpp>
-#include <Geode/loader/Loader.hpp>
-#include <algorithm>
-#include <cctype>
-#include <set>
-#include <iostream>
+#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
-
-class buttonfunctionthing : public CCLayer {
-public:
-    void onButtonClick(CCObject* sender) {
-        FLAlertLayer::create(
-            "About",
-            "This badge is given to <cp>members of the Demon List team</c>.",
-            "OK"
-        )->show();
-    }
-};
+bool downloaded = false;
+std::set<std::string> demonlistMembers;
 
 class $modify(ProfilePage) {
-    bool m_hasBeenOpened = false;
 
-    bool init(int p0, bool p1) {
-        if (!ProfilePage::init(p0, p1)) return false;
+CCSprite * icon;
 
-        // URL of the raw text file on GitHub
-        std::string githubFileUrl = "https://raw.githubusercontent.com/ArkaneOnline/dl-team-icons/main/dl-team.txt";
+	void setupPageInfo(gd::string name, char const* chars) {
+		ProfilePage::setupPageInfo(name,chars);
 
-        // Download the content of the text file using Geode's web request
-        auto res = web::fetch(githubFileUrl);
-        if (!res) {
-            std::cerr << "Failed to fetch file from GitHub." << std::endl;
-            return false;
-        }
+		auto director = CCDirector::sharedDirector();
+		auto size = director->getWinSize();
 
-        // Get the content from the response
-        std::string fileContent = res.value();
-
-        // Parse the content and populate the set
-        std::set<int> demonlistMembers;
-        std::istringstream iss(fileContent);
-        int contributorId;
-
-        while (iss >> contributorId) {
-            demonlistMembers.insert(contributorId);
-        }
-
-        m_mainLayer->removeChildByID("my-stuff-hint");
-        auto mainMenu = static_cast<cocos2d::CCLayer*>(this->getChildByIDRecursive("main-menu"));
-        auto spr = CCSprite::create("demonlist.png"_spr);
-        auto btn = CCMenuItemSpriteExtra::create(
-            spr, this, menu_selector(buttonfunctionthing::onButtonClick)
-        );
-        auto contentSize = mainMenu->getContentSize();
-
-		auto posX = m_usernameLabel->getPositionX();
-        auto posY = m_usernameLabel->getPositionY();
-
-		std::string username = m_usernameLabel->getString();
-
-        btn->setPosition(15, -50);
-        btn->setZOrder(26);
-        btn->setID("demonlist-badge");
-        mainMenu->addChild(btn);
-        btn->setVisible(false);
-
-        if (demonlistMembers.count(p0) > 0) {
-            btn->setVisible(true);
-        }
-        else {
-            // how many times do we have to tell you this old man
-        }
-
+		if (!downloaded) {
+			auto res = web::fetch("https://raw.githubusercontent.com/ArkaneOnline/dl-team-icons/main/dl-team.txt");
+			if (!res) {
+				log::info("Failed to fetch file from GitHub.");
+			}
+			auto data = res.value();
         
+			std::istringstream iss(data);
+			std::string temp_string;
+			log::info("Downloaded Demon List team member list");
 
-        return true;
-    }
+			while (std::getline(iss, temp_string)) {
+                demonlistMembers.insert(temp_string);
+            }
+			downloaded = true;
+		}
+
+		std::string player_name = m_usernameLabel->getString();
+
+		for (const auto& names : demonlistMembers) {
+			if (names == player_name) {
+				log::info("DL Member Found! {}", names);
+				CCNode* first_letter = reinterpret_cast<CCNode*>(m_usernameLabel->getChildren()->objectAtIndex(0));
+				m_fields->icon = CCSprite::create("demonlist.png"_spr);
+				m_fields->icon->setScale(m_usernameLabel->getScale() - 0.1);
+				m_fields->icon->setPosition(first_letter->convertToWorldSpace(getPosition()));
+				m_fields->icon->setPosition({m_fields->icon->getPositionX() - 11.f, m_fields->icon->getPositionY() + 11.f});
+				addChild(m_fields->icon);
+			}
+		}
+	} 
 };
